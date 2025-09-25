f695fddcfece722c623fda465f82619bee69bb38
AmireuxLuna <amireux_jiao@163.com>
2025-09-25 15:14:01 +0800

    ✨ (feat): basic 广度优先搜索算法完成

--- cmd/7-ConcurrentGraphBFSQueries/07-ConcurrentGraphBFSQueries.go
+++ cmd/7-ConcurrentGraphBFSQueries/07-ConcurrentGraphBFSQueries.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 // ConcurrentBFSQueries concurrently processes BFS queries on the provided graph.
 // - graph: adjacency list, e.g., graph[u] = []int{v1, v2, ...}
 // - queries: a list of starting nodes for BFS.
@@ -15,4 +19,53 @@ func ConcurrentBFSQueries(graph map[int][]int, queries []int, numWorkers int) ma
 
 func main() {
 	// You can insert optional local tests here if desired.
+	graph := map[int][]int{
+		0: {1, 2},
+		1: {0, 3, 4},
+		2: {0, 5},
+		3: {1, 5},
+		4: {1, 5},
+		5: {2, 3, 6, 7},
+		6: {5},
+		7: {5},
+		8: {5},
+		9: {8},
+	}
+	_, path := standardBFS(graph, 0, 8)
+	fmt.Println(path)
+}
+
+func standardBFS(graph map[int][]int, start int, goal int) (bool, map[int][]int) {
+	queue := []int{start}
+	path := make(map[int][]int)
+	visited := make(map[int]bool)
+
+	// init start node infomation
+	visited[start] = true
+	path[start] = []int{start}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		if node == goal {
+			return true, path
+		}
+
+		for _, neighbor := range graph[node] {
+			if !visited[neighbor] {
+				visited[neighbor] = true
+
+				// * note: output : [xx, xx, xx, xx, <not have goal>]
+				// path[neighbor] = append(path[node], node)
+
+				// * note: output : [xx, xx, xx, xx, <have goal>]
+				newPath := append(append([]int(nil), path[node]...), neighbor)
+				path[neighbor] = newPath
+
+				queue = append(queue, neighbor)
+			}
+		}
+	}
+	return false, path
 }
